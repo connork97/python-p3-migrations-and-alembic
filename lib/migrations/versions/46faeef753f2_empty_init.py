f6ff4f744ae92eae0809d29c3bd6d22a8388cf04
Connor Kormos <connorkormos97@gmail.com>
2023-04-17 17:39:41 -0700

    complete

---
+++ lib/migrations/versions/46faeef753f2_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 46faeef753f2
+Revises: 
+Create Date: 2023-04-17 17:25:47.668750
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '46faeef753f2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
